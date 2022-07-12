682c592e738f6fdd907f27c7d8eff8a6c0e9ec51
pradana <pradanamahendra@gmail.com>
2022-07-12 13:47:38 +0700

    https ajax

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -174,7 +174,7 @@
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 
     <script src="{{ asset('js/foruser.js') }}"></script>
     <script src="{{ asset('aos-master/dist/aos.js') }}"></script>
